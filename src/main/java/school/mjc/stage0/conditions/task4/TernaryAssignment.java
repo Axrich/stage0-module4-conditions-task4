ade733faa428d7a1c6c8aeca885efe2371088f36
Axrich <116589250+Axrich@users.noreply.github.com>
2023-12-19 15:12:23 +0500

    Update TernaryAssignment.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result = (fist > second) ? 10 : -10;
     }
+    public static void main(String[] args) {
+        TernaryAssignment ternaryAssignment = new TernaryAssignment();
+        ternaryAssignment.assignAndPrintBasedOnWhichBigger(15 ,8);
+}
 }
