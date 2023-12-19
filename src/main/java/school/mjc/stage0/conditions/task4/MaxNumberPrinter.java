467b14a9cf1930cef69073e4c92fccd90bb48df2
Axrich <116589250+Axrich@users.noreply.github.com>
2023-12-19 14:36:47 +0500

    Update MaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first >= second && third >= third) {
+            System.out.println("The greatest number is: " + first);
+            { else if (second >= first && second >= third) {
+                System.out.println("The greatest number is: + second);
+                { else {
+                    System.out.println("The greatest number is: + third);
     }
 }
+
+                public static void main(String[] args) {
+                    MaxNumberPrinter MaxNumberPrinter = new MaxNumberPrinter();
+                    maxNumberPrinter.printGreatest(3, 7, 12);
+                }
+            }
