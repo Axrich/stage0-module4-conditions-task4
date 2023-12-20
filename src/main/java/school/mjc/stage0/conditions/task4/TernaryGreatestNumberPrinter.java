7d23e6000c0cf4dce4f49ae43a490fe4e5555b32
Axrich <116589250+Axrich@users.noreply.github.com>
2023-12-20 22:24:34 +0500

    Update TernaryGreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -1,13 +1,13 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
-        int greatest = (first >= second) ? first : second;
+    public void printGreatest(int first, int second, int third) {
+        int greatest = (first >= second && first >= third) ? first : (second >= third) ? second : third;
         System.out.println("Greatest number: " + greatest);
     }
 
     public static void main(String[] args) {
         TernaryGreatestNumberPrinter printer = new TernaryGreatestNumberPrinter();
-        printer.printGreatest(1, 0);
+        printer.printGreatest(1, 0, -1);
     }
 }
